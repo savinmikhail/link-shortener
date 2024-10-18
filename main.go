172cca63424bd5871a3498198e1da0495be2c251
Mikhail <salazar290720035017@gmail.com>
2024-10-18 21:38:11 +0700

    add 2 endpoints prototypes

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+import "encoding/json"
+
+func shorten(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	url := r.PostForm.Get("url")
+	shortUrl := md5.Sum([]byte(url))
+	mappedUrls := make(map[string]string)
+	mappedUrls[string(shortUrl[:])] = url
+	jsonContent, err := json.Marshal(mappedUrls)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	os.WriteFile("list.json", jsonContent, 0777)
+	fmt.Fprintf(w, "saved %v", url)
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	shortCode := r.URL.Path[1:]
+	jsonContent, err := os.ReadFile("list.json")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	mappedUrls := make(map[string]string)
+	err1 := json.Unmarshal(jsonContent, &mappedUrls)
+	if err1 != nil {
+		fmt.Fprintf(w, err1.Error())
+	}
+	origUrl := mappedUrls[shortCode]
+	http.Redirect(w, r, origUrl, http.StatusFound)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/shorten", shorten)
+	http.HandleFunc("/", redirect)
+	//http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":8079", nil))
 }
