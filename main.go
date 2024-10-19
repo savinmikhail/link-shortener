81b705f8b381deab0ca6313777487ca3dfe8b498
Mikhail <salazar290720035017@gmail.com>
2024-10-19 09:29:47 +0700

    fix url being empty and short code being raw bytes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,44 +10,95 @@ import (
 )
 import "encoding/json"
 
+type RequestData struct {
+	URL string `json:"url"`
+}
+
 func shorten(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	// get orig url
+	var data RequestData
+	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-	url := r.PostForm.Get("url")
-	shortUrl := md5.Sum([]byte(url))
-	mappedUrls := make(map[string]string)
-	mappedUrls[string(shortUrl[:])] = url
+	url := data.URL
+	if url == "" {
+		http.Error(w, "URL is required", http.StatusBadRequest)
+		return
+	}
+	//get short url
+	hash := md5.Sum([]byte(url))
+	stringHash := hex.EncodeToString(hash[:])
+	shortUrl := stringHash[:8]
+	//write to the file
+	mappedUrls, err := getMappedUrls()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	mappedUrls[shortUrl] = url
+	err = saveMappedUrls(mappedUrls)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	//respond
+	fmt.Fprintf(w, "saved %v", url)
+	fmt.Fprintf(w, "\n short code: http://localhost:8079/%v", shortUrl)
+}
+
+func saveMappedUrls(mappedUrls map[string]string) error {
 	jsonContent, err := json.Marshal(mappedUrls)
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
-		return
+		return err
 	}
-	os.WriteFile("list.json", jsonContent, 0777)
-	fmt.Fprintf(w, "saved %v", url)
+	err = os.WriteFile("list.json", jsonContent, 0777)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
 	shortCode := r.URL.Path[1:]
-	jsonContent, err := os.ReadFile("list.json")
+	origUrl, err := getOrigUrlByShortCode(shortCode)
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	http.Redirect(w, r, origUrl, http.StatusFound)
+}
+
+func getOrigUrlByShortCode(shortCode string) (string, error) {
+	mappedUrls, err := getMappedUrls()
+	if err != nil {
+		return "", err
+	}
+	origUrl, exists := mappedUrls[shortCode]
+	if !exists {
+		return "", fmt.Errorf("short code %s not found", shortCode)
+	}
+	return origUrl, nil
+}
+
+func getMappedUrls() (map[string]string, error) {
+	fileContent, err := os.ReadFile("list.json")
+	if err != nil {
+		return nil, err
 	}
 	mappedUrls := make(map[string]string)
-	err1 := json.Unmarshal(jsonContent, &mappedUrls)
-	if err1 != nil {
-		fmt.Fprintf(w, err1.Error())
+	err = json.Unmarshal(fileContent, &mappedUrls)
+	if err != nil {
+		return nil, err
 	}
-	origUrl := mappedUrls[shortCode]
-	http.Redirect(w, r, origUrl, http.StatusFound)
+	return mappedUrls, nil
 }
 
 func main() {
 	http.HandleFunc("/shorten", shorten)
 	http.HandleFunc("/", redirect)
-	//http.HandleFunc("/", handler)
 
 	log.Fatal(http.ListenAndServe(":8079", nil))
 }
