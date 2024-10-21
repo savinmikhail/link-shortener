51f9048749f4816d87d498653744d21d170a65b1
Mikhail <salazar290720035017@gmail.com>
2024-10-21 12:53:06 +0700

    remove initializers

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"github.com/savinmikhail/go_crud/initializers"
-	"github.com/savinmikhail/link-shortener/models"
-)
-
-func init() {
-	initializers.LoadEnvVariables()
-	initializers.ConnectToDB()
-}
-
 func main() {
-	initializers.DB.AutoMigrate(&models.ShortLink{})
+
 }
